4059a6809e974a3fd51bca3a49fc1e504cac38eb
ankitjha123456 <142203109+ankitjha123456@users.noreply.github.com>
2026-04-09 09:57:11 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-
-#Check input
+# Check input
 if [ "$#" -ne 4 ]; then
-        echo "Usage:  $0 <server> <broker> <eg_name> <service_name>"
+        echo "Usage: $0 <server> <broker> <eg_name> <service_name>"
         return 1
 fi
 
@@ -12,24 +11,36 @@ broker=$2
 eg=$3
 service=$4
 
-
 REMOTE_PATH="/var/mqsi/components/${broker}/servers/${eg}/run/${service}"
 
-
 ssh -tq "$server" "
+        cd $REMOTE_PATH || { echo '{\"error\": \"Path not found\"}'; exit 1; }
 
-        cd $REMOTE_PATH || { echo 'Path not found'; exit 1; }
+        # Build JSON manually
+        first=1
+        result='{'
 
-
-        # Find all .xsd file and cat them
         for file in *.xsd; do
-                #pwd
-                #ls
                 if [ -e \"\$file\" ]; then
-                        cat \"\$file\"
+                        # Get filename without extension as key
+                        key=\$(basename \"\$file\" .xsd)
+
+                        # Read file content and escape special JSON characters
+                        content=\$(cat \"\$file\" | sed 's/\\\\/\\\\\\\\/g; s/\"/\\\\\"/g; s/\t/\\\\t/g' | tr -d '\r' | awk '{printf \"%s\\\\n\", \$0}' | tr -d '\n')
+
+                        if [ \"\$first\" -eq 1 ]; then
+                                first=0
+                        else
+                                result=\"\$result,\"
+                        fi
+
+                        result=\"\$result\\\"'\$key'\\\":\\\"'\$content'\\\"\"
                 else
-                        echo 'No validation files around'
+                        echo '{\"error\": \"No validation files found\"}'
                         exit 1
                 fi
         done
+
+        result=\"\$result}\"
+        echo \"\$result\"
 "
